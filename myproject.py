f3fb93511ef6403a6dad2309eaa5bdb688127a1a
hamed <60010552+h-soleymani@users.noreply.github.com>
2021-09-05 09:26:47 +0430

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -1,8 +1,10 @@
-from functions import list_to_excel
+from functions import list_to_excel, search,send_email
 import pandas as pd
 
 my_data = pd.read_excel('data1.xlsx')
+profile_df = pd.read_excel('profile.xlsx')
 my_list = my_data.values.tolist()
+profile_list = profile_df.values.tolist()
 my_header = my_data.columns.tolist()
 headers =[]
 for item in my_header:
@@ -21,3 +23,4 @@ for item in unique_type:
             temp.append(row)
 
     list_to_excel(temp,headers,item)
+    send_email(item,search(profile_list,item),'')
