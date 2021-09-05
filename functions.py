ebaf757b167b32dbb6c70f34516f3c188c9ac77b
hamed <60010552+h-soleymani@users.noreply.github.com>
2021-09-05 09:25:01 +0430

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import xlsxwriter
+import win32com.client
 
 
 def list_to_excel(list_name,headers, file_name):
@@ -9,10 +10,33 @@ def list_to_excel(list_name,headers, file_name):
         last_c = len(list_name[0])-1
     else:
         last_c = 0
-    workbook = xlsxwriter.Workbook(file_name + '.xlsx')
+    workbook = xlsxwriter.Workbook('export/' + file_name + '.xlsx')
     worksheet = workbook.add_worksheet()
     worksheet.add_table(first_r, first_c, last_r, last_c, {'data': list_name,'columns':headers,
                                                            'style': 'Table Style Light 11'})
     workbook.close()
 
+
+
+def send_email (name, recipient, cc):
+    outlook = win32com.client.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = recipient
+    mail.bcc = 'h.soleymani@asiatech.ir'
+    mail.Subject = name
+    mail.HTMLBody = '<h3>This is HTML Body</h3>'
+    # mail.Attachments.Add('export/' + name + '.xlsx')
+    mail.Attachments.Add('C:/Users/h.soleymani/Desktop/projects/split excel and send email/export/' + name + '.xlsx')
+    mail.CC = cc
+    mail.Send()
+
+
+
+def search (list,name):
+   
+    for item in list:
+        if item[0]==name:
+            # print(item[1])
+            return item[1]
+    return ""    
 
